6a372424b1f1e7b4d2ca9be54d6886cb8d2259c0
VITOR <VITOR@VITORC>
2020-01-21 17:41:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace My_First_GIT_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("This is ISM 4300");
+        }
+    }
+}
